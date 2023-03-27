02d745e184a8d4f03711094a66a141fab652c62a
Aurelien Allenic <aurelien.allenic@gmail.com>
2023-03-27 17:13:06 +0200

    (feat) added logic for ustencils filter to remove occurencies

--- scripts/filters.js
+++ scripts/filters.js
@@ -1,7 +1,7 @@
 import { recipes } from "../data/data.js"
 
 let filters = document.getElementById('section-filters')
-
+let chars = new Set()
 filters.innerHTML = `
 <span class="hidden_button">
 <button type="button"></button>
@@ -73,6 +73,7 @@ function displayAppareils(){
     sectionAppareil.appendChild(ul)
     for(let i = 0; i < recipes.length; i++){
         let recipeAppareils = recipes[i].appliance;
+
         let li = document.createElement('li');
         li.innerHTML = `${recipeAppareils}`
         ul.appendChild(li)
@@ -92,35 +93,58 @@ function displayAppareils(){
 let ArrowAppareils = document.getElementById("arrow_appareils")
 ArrowAppareils.addEventListener('click', displayAppareils)
 
+
+function removeOccurencies(){
+    let newArrayIng = [];
+    for(let i = 0; i < recipes.length; i++){
+        let recipeIngredients = recipes[i].ingredients;
+        
+        
+        recipeIngredients.forEach(ing => {
+
+           newArrayIng.push(ing.ingredient);
+        })
+            
+    }
+    chars = new Set(newArrayIng)
+    return chars
+}
+
+
 let isOpenUstensils = false;
-function displayUstensils(){
+async function displayUstensils(){ 
+    await removeOccurencies(chars)
     if(isOpenUstensils === false){
     let sectionUstensils = document.getElementById('container_hidden_options_ustensils')
     sectionUstensils.style.display = "inherit"
     let ul = document.createElement('ul');
     ul.setAttribute("class", "container_hidden_filter_ustensils")
     sectionUstensils.appendChild(ul)
-    for(let i = 0; i < recipes.length; i++){
-        let recipeIngredients = recipes[i].ustensils;
-        recipeIngredients.forEach(ust => {
-            let li = document.createElement('li');
-            li.innerHTML = `${ust}`
-            ul.appendChild(li)
-            
+    console.log(chars)
+            for(let value of chars){
+                console.log(value)
+                let li = document.createElement('li');
+                li.innerHTML = `${value}`
+                ul.appendChild(li)
+                
+    
+                let sectionIngredients = document.getElementById("container_hidden_options_ingredients")
+                sectionIngredients.style.display = "none"
+                let sectionAppareils = document.getElementById("container_hidden_options_appareils")
+                sectionAppareils.style.display = "none"
+                isOpenUstensils = true
+        }
+    
+        } else {
+            let sectionUstensils = document.getElementById('container_hidden_options_ustensils')
+            sectionUstensils.style.display = "none"
+            isOpenUstensils = false
+        }
+    }
+    
+   
 
-            let sectionIngredients = document.getElementById("container_hidden_options_ingredients")
-            sectionIngredients.style.display = "none"
-            let sectionAppareils = document.getElementById("container_hidden_options_appareils")
-            sectionAppareils.style.display = "none"
-            isOpenUstensils = true
-        });
-    }
-    }else {
-        let sectionUstensils = document.getElementById('container_hidden_options_ustensils')
-        sectionUstensils.style.display = "none"
-        isOpenUstensils = false
-    }
-}
 
 let ArrowUstensils = document.getElementById("arrow_ustensils")
 ArrowUstensils.addEventListener('click', displayUstensils)
+
